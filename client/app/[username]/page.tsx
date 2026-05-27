10a0603a6d1d55e34ef7df1baffbd1d4aa954cb5
unluckynilesh <nileshinde001@gmail.com>
2026-05-27 16:11:57 +0530

    avatar as const

--- client/app/[username]/page.tsx
+++ client/app/[username]/page.tsx
@@ -54,7 +54,7 @@ const avatarVariants = {
   animate: {
     opacity: 1,
     scale: 1,
-    transition: { duration: 0.5, ease: [0.22, 1, 0.36, 1] },
+    transition: { duration: 0.5, ease: [0.22, 1, 0.36, 1] as const },
   },
 };
 
